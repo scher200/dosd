7900a832eb81c81e8bf7081114f50ed8e8ee52c7
scher200 <jeroenschermer@gmail.com>
2018-03-21 15:26:31 +0100

    added CUSTOM_CMD variable option

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 : ${DOCKERD_CMD:="dockerd --log-level 'fatal' > /dev/null 2>&1 &"}
+: ${CUSTOM_CMD:="echo ok"}
 : ${GIT_REPO:="https://github.com/openfaas/faas"}
 : ${GIT_BRANCH:="0.7.1"}
 : ${DEPLOY_STACK:="./deploy_stack.sh"}
 
 bash -c "$DOCKERD_CMD" &
 
-sleep 3 && docker swarm init
+sleep 3 && docker swarm init && ${CUSTOM_CMD}
 
 git clone -v $GIT_REPO repo \
  && cd repo \
